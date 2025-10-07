4cdcb42b1353423b7e559b382deb85b092f04631
zhaokuang <zhaokuangDK@163.com>
2025-10-08 01:19:13 +0800

    fix:标准化chatparams的变量名

--- internal/config/llm_config.go
+++ internal/config/llm_config.go
@@ -9,14 +9,14 @@ type LLMConfig struct {
 }
 
 type ChatParams struct {
-  SystemPrompt         string  `json:"system_prompt"`
-  MaxPromptContent     int     `json:"max_prompt_content"`
-	MaxTokensToPredicted int     `json:"max_tokens_to_predicted"`
-	Temperature          float64 `json:"temperature"`
-	TopP                 float64 `json:"top_p"`
-	FrequencyPenalty     float64 `json:"frequency_penalty"`
-	PresencePenalty      float64 `json:"presence_penalty"`
-	RepeatPenalty        float64 `json:"repeat_penalty"`
+  Prompt           string  `json:"prompt"`
+  ContentSize      int     `json:"content_size"`
+	MaxTokens        int     `json:"max_tokens"`
+	Temperature      float64 `json:"temperature"`
+	TopP             float64 `json:"top_p"`
+	FrequencyPenalty float64 `json:"frequency_penalty"`
+	PresencePenalty  float64 `json:"presence_penalty"`
+	RepeatPenalty    float64 `json:"repeat_penalty"`
 }
 
 var (
@@ -27,19 +27,19 @@ var (
 func InitLLMConfig() {
 	LLMConfigure = LLMConfig{
 		ApiUrl:             getEnv("LLM_API_URL", "http://ai.api.maybered.com/AI-VMZ-8B/v1/chat/completions"),
-		ApiKey:             getEnv("LLM_API_KEY", ""),
+		ApiKey:             getEnv("LLM_API_KEY", "Kviagb.diagb2"),
 		MaxConcurrentTasks: getEnvInt("LLM_MAX_CONCURRENT_TASKS", 6),
 		Timeout:            getEnvInt("LLM_TIMEOUT", 20),
 		AvailableModels:    []string{"AI-VMZ-8B"},
 	}
 	ChatParameters = ChatParams{
-		SystemPrompt:         getEnv("LLM_SYSTEM_PROMPT", "你是一个AI助手，你的知识广泛，尤其精通哲学、历史、文学等人文学科，你需要以严谨而逻辑清晰的方式响应用户提问。"),
-		MaxPromptContent:     getEnvInt("LLM_MAX_PROMPT_CONTENT", 8192),
-		MaxTokensToPredicted: getEnvInt("LLM_MAX_TOKENS_TO_PREDICTED", 2048),
-		Temperature:          getEnvFloat("LLM_TEMPERATURE", 0.3),
-		TopP:                 getEnvFloat("LLM_TOP_P", 0.7),
-		FrequencyPenalty:     getEnvFloat("LLM_FREQUENCY_PENALTY", 0.0),
-		PresencePenalty:      getEnvFloat("LLM_PRESENCE_PENALTY", 0.0),
-		RepeatPenalty:        getEnvFloat("LLM_REPEAT_PENALTY", 1.05),
+		Prompt:           getEnv("LLM_PROMPT", "你是一个AI助手，你的知识广泛，尤其精通哲学、历史、文学等人文学科，你需要以严谨而逻辑清晰的方式响应用户提问。"),
+		ContentSize:      getEnvInt("LLM_CONTENT_SIZE", 8192),
+		MaxTokens:        getEnvInt("LLM_MAX_TOKENS", 2048),
+		Temperature:      getEnvFloat("LLM_TEMPERATURE", 0.3),
+		TopP:             getEnvFloat("LLM_TOP_P", 0.7),
+		FrequencyPenalty: getEnvFloat("LLM_FREQUENCY_PENALTY", 0.0),
+		PresencePenalty:  getEnvFloat("LLM_PRESENCE_PENALTY", 0.0),
+		RepeatPenalty:    getEnvFloat("LLM_REPEAT_PENALTY", 1.05),
 	}
 }
